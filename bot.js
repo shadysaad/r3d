5ad023141db0ab34e107a41597213ec2b05abfab
shadysaad <45115969+shadysaad@users.noreply.github.com>
2019-08-25 15:37:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,13 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
+      console.log(`Cyhper Bot Script By : r3d`);
         console.log(`----------------`);
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`TEXT`,"https://www.twitch.tv/ra3dal3arb")
 client.user.setStatus("dnd")
 });
 
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+client.login("NDA0MjkyMzk1MzEyODczNDcz.XVhabg.R-0yW-7PMTpttvf8V3x1F9mIZ9c");// لا تغير فيها شيء
